0e115de9af4bd1a8e97b5edf786cfc991f5571b5
william chang <wwwy3y3@gmail.com>
2018-11-15 11:36:41 +0800

    export plugins

---
+++ src/plugins/index.ts
@@ -0,0 +1,11 @@
+
+// export interface
+export { Plugin } from './interface';
+
+// export implementation
+export { default as BaseTypePlugin } from './baseType';
+export { default as WhereInputPlugin } from './whereInput';
+export { default as QueryPlugin } from './query';
+export { default as CreatePlugin } from './create';
+export { default as UpdatePlugin } from './update';
+export { default as DeletePlugin } from './delete';
