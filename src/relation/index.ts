39ea7ff9b968da05c0484e8cedf772cd718f389a
william chang <wwwy3y3@gmail.com>
2018-11-23 18:10:16 +0800

    export relations

---
+++ src/relation/index.ts
@@ -0,0 +1,5 @@
+
+export { default as UniToOneRelation } from './uniToOne';
+export { default as BiOneToOneRelation } from './biOneToOne';
+export { default as OneToManyRelation } from './oneToMany';
+export { default as ManyToManyRelation } from './manyToMany';
