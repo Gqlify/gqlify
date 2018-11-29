afaf86f103c203cc77fbfe32d1c7935d319a43e9
william chang <wwwy3y3@gmail.com>
2018-11-29 16:12:11 +0800

    export index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 
 // gqlify
 export { Gqlify } from './gqlify';
+
+// export data source
+export * from './dataSource/interface';
+
+// export helpers
+export * from './helper';
+
+// plugins
+export * from './plugins';
